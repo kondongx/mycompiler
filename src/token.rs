f33dd04528608975f36d78a9bab3d47e53582bd3
kondongx <kondongx@gmail.com>
2021-11-25 21:01:29 +0800

    trait Token support Clone

--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,27 @@
+use std::fmt::Debug;
+
 use crate::token_type::TokenType;
 
-pub trait Token {
+pub trait Token: TokenClone + Debug {
     fn get_type(&self) -> &TokenType;
     fn get_text(&self) -> &str;
+}
+
+pub trait TokenClone {
+    fn clone_box(&self) -> Box<dyn Token>;
+}
+
+impl<T> TokenClone for T
+where
+    T: 'static + Token + Clone,
+{
+    fn clone_box(&self) -> Box<dyn Token> {
+        Box::new(self.clone())
+    }
+}
+
+impl Clone for Box<dyn Token> {
+    fn clone(&self) -> Box<dyn Token> {
+        self.clone_box()
+    }
 }
